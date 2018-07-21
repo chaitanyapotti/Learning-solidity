dd9ea76ff0ef2405b7ad9df447c3a052e5678b50
chaitanya potti <chaitanya.potti@gmail.com>
2018-07-21 16:39:07 +0530

    comment update

--- compile.js
+++ compile.js
@@ -5,4 +5,5 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+//Can feed file path to solc and use contracts['filename:contractname'];
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
