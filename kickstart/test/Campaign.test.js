92a5afcbd917b6c1dc97cda5bf33bafe369c4354
chaitanyapotti <chaitanya@electus.network>
2018-09-02 16:50:20 +0530

    deployed

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -20,5 +20,74 @@ beforeEach(async () => {
       gas: "1000000"
     });
 
-  //await factory.methods.
+  await factory.methods.createCampaign("100").send({ from: acccounts[0] });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
 });
+
+describe("Campaigns", () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+  it("marks caller as campaign manager", async () => {
+    const manager = await campaign.methods.manager.call();
+    assert.equal(acccounts[0], manager);
+  });
+
+  it("allows people to contribute money and marks them as approvers" async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[1], value: "200"
+    });
+    const isApprover = await campaign.methods.approvers(accounts[1]).call();
+    assert(isApprover);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        from: acccounts[1], value: "99"
+      });
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
+
+  it("allows a manager to make a payment request", async () => {
+    await campaign.methods.createRequest("buy batteries", "100", acccounts[1]).send({
+      from: accounts[0], gas: "1000000"
+    });
+
+    const request = await campaign.methods.requests(0).call();
+    assert.equal("buy batteries", request.description);
+  });
+
+  it("process requests", async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0], value: web3.utils.toWei("10", "ether")
+    });
+
+    await campaign.methods.createRequest("A",web3.utils.toWei("5", "ether"), accounts[1]).send({
+      from: accounts[0], gas: "1000000"
+    });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0], gas: "1000000"
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0], gas: "1000000"
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    balance = parseFloat(balance);
+
+    assert(balance > 104);
+  });
+
+})
