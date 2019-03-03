cb2a9e7e46ac01c6fe1b32cb40e29a56980bb306
chaitanyapotti <chaitanya@electus.network>
2019-03-03 14:37:48 +0530

    pinned packages and fixed scripts

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -1,30 +1,35 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const compiledFactory = require("./build/CampaignFactory.json");
-//FYI.. No real ether in these accounts.! :P
+// FYI.. No real ether in these accounts.! :P
 const provider = new HDWalletProvider(
   "blue inherit drum enroll amused please camp false estate flash sell right",
   "https://rinkeby.infura.io/v3/dc22c9c6245742069d5fe663bfa8a698"
 );
 
-const web3 = new Web3(provider);
+const deploy = async web3 => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log("preparing to deploy from : ", accounts[0]);
 
-const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-
-  console.log("preparing to deploy from : ", accounts[0]);
-
-  const campaignFactory = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface)
-  )
-    .deploy({
-      data: "0x" + compiledFactory.bytecode
-    })
-    .send({
-      from: accounts[0],
-      gas: 2000000
-    });
-  console.log("Contract deployed at ", campaignFactory.options.address);
+    const campaignFactory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+      .deploy({
+        data: `0x${compiledFactory.bytecode}`
+      })
+      .send({
+        from: accounts[0],
+        gas: 2000000
+      })
+      .catch(err => console.log(err));
+    console.log("Contract deployed at ", campaignFactory.options.address);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-deploy();
+try {
+  const web3 = new Web3(provider);
+  deploy(web3);
+} catch (error) {
+  console.log(error);
+}
--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
-const address = "0x";
+const address = "0x6c1f75450E2164095b61CC0A70b9b1154f814a07";
 
 const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), address);
 
